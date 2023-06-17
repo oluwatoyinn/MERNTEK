0879fc52793feb360d9aeaf150cd14f7f804326b
Victor Ajayi <ovajayi@gmail.com>
2023-06-17 11:46:23 +0100

    complete login, get user and update user route backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import generateToken from "./../utils/generateToken.js";
 //POST /api/users/auth
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //user register new user
@@ -44,7 +58,11 @@ const registerUser = asyncHandler(async (req, res) => {
 //POST /api/users public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User Successfully logged out" });
 });
 
 //user get user profile
@@ -52,7 +70,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 //PRIVATE
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 //user UPDATE user profile
@@ -60,6 +83,26 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //PRIVATE
 
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
   res.status(200).json({ message: "Update User Profile" });
 });
 
